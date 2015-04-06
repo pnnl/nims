0d0bfb3d7f00bd5823bd15ad93a0882eeeb67860
d3x389 <d3x389@e0feaedf-03d4-4201-b3fb-2d10bec3ea31>
2015-04-06 15:04:14 +0000

    added inotify to watch directory for files
    git-svn-id: https://subversion.pnnl.gov/svn/NIMS/trunk@4 e0feaedf-03d4-4201-b3fb-2d10bec3ea31

--- ingester.cpp
+++ ingester.cpp
@@ -8,20 +8,29 @@
  *
  */
 #include <iostream> // cout, cin, cerr
-//#include <fstream>  // ifstream, ofstream
+#include <fstream>  // ifstream, ofstream
 #include <string>   // for strings
+#include <sys/inotify.h> // watch a directory
 
 //#include <opencv2/opencv.hpp>
 
-//#include <boost/filesystem.hpp>
+#include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 
 using namespace std;
 //using namespace cv;
 using namespace boost;
 namespace po = boost::program_options;
-//namespace fs = boost::filesystem;
+namespace fs = boost::filesystem;
 
+#define EVENT_SIZE ( sizeof(struct inotify_event) )
+#define BUF_LEN ( 1024 * (EVENT_SIZE + 16) )
+
+void cleanupiNotify(int fd, int wd)
+{
+    ( void ) inotify_rm_watch( fd, wd );
+    ( void ) close( fd );
+}
 
 int main (int argc, char * const argv[]) {
 	//--------------------------------------------------------------------------
@@ -30,8 +39,7 @@ int main (int argc, char * const argv[]) {
 	po::options_description desc;
 	desc.add_options()
 	("help",                                                    "print help message")
-	("foo,f", po::value<string>()->default_value( "" ),         "a string value")
-	("bar,b",   po::value<unsigned int>()->default_value( 101 ),"an integer value")
+	("indir,i", po::value<string>()->default_value( "/home/input" ), "path for input files")
 	;
 	po::variables_map options;
     try
@@ -53,12 +61,76 @@ int main (int argc, char * const argv[]) {
         return 0;
     }
 	
-    string foo = options["foo"].as<string>();
-	unsigned int bar = options["bar"].as<unsigned int>();
+    string inputpath = options["indir"].as<string>();
     
 	//--------------------------------------------------------------------------
 	// DO STUFF
 	cout << endl << "Starting " << argv[0] << endl;
+	
+	// TODO: make sure input path exists
+    clog << "Watching directory " << inputpath << endl;
+	
+	// from Beluga.cpp (Michael Henry)
+	// This is a semi-complicated watch that we're setting up
+    //  When data comes in, it's binned based on the hour
+    //  At the top of a new hour, a directory for that hour is
+    //  created, and data is copied to that directory
+    //  So, what we need to do is monitor a top-level directory
+    //  for the creation of new subdirectories. Once we detect
+    //  that event, we then need to monitor that subdirectory for
+    //  new files. Simple, right?
+    char buffer[BUF_LEN];
+    string watchDirectory = "";
+
+    while( 1 ) {
+		
+        int fd = inotify_init();
+		
+        if( fd < 0 ) {
+            perror( "Error initializing inotify" );
+            break;
+        }
+		
+        int wd;
+        if( watchDirectory != "" ) {
+            // watch for newly created files
+            wd = inotify_add_watch( fd, watchDirectory.c_str(), IN_CLOSE_WRITE );
+        }
+        // watch for newly created directories
+        int cd = inotify_add_watch( fd, inputpath.c_str(), IN_CREATE );
+		
+        int length = read( fd, buffer, BUF_LEN );
+		
+        if( length < 0 ) {
+            perror( "Error reading newly created file" );
+            cleanupiNotify(fd,wd);
+            cleanupiNotify(fd,cd);
+            continue;
+        }
+		
+        int i=0;
+        while( i < length ) {
+            struct inotify_event *event = ( struct inotify_event * ) &buffer[i];
+            if( event->len ) {
+                if( event->mask & IN_CREATE ) {
+                    if( event->mask & IN_ISDIR ) {
+                        watchDirectory = inputpath + "/" + event->name;
+                        clog << "Found new directory: " << watchDirectory << endl;
+                    }
+                } 
+                if( event->mask & IN_CLOSE_WRITE ) {
+                    if( !(event->mask & IN_ISDIR) ) {
+                        clog << "Found file: " << event->name << endl;
+                        //processFile( watchDirectory, event->name, jw );
+                    }
+                }
+            }
+            i += EVENT_SIZE + event->len;
+        }
+        cleanupiNotify( fd, wd );
+        cleanupiNotify( fd, cd );
+		
+    }
 	
     
 	cout << endl << "Ending " << argv[0] << endl << endl;
