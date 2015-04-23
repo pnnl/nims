99e74ff4bb9c78bbd54e5d81df74757990f80cdf
d3m570 <d3m570@e0feaedf-03d4-4201-b3fb-2d10bec3ea31>
2015-04-23 17:41:35 +0000

    initial version of shell scripts for build/install
    git-svn-id: https://subversion.pnnl.gov/svn/NIMS/trunk@21 e0feaedf-03d4-4201-b3fb-2d10bec3ea31

---
+++ scripts/cmake-nims.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+BUILD_DIR=../../nims-build
+SOURCE_DIR=../nims-source
+YAML_SRC=vendorsrc/yaml-cpp-release-0.5.2
+
+if ! [ -d "$YAML_SRC" ]; then
+    echo "*** checking out and configuring vendorsrc ***"
+    svn co https://subversion.pnnl.gov/svn/NIMS/vendorsrc
+fi
+
+if ! [ -d "$YAML_SRC/build" ]; then
+    echo "*** configuring yaml-cpp ***"
+    pushd .
+    cd "$YAML_SRC"
+    mkdir build
+    cd build
+    cmake ../ && make
+    echo "*** You now need to run 'sudo make install' in $(pwd) ***"
+    exit 0
+fi
+
+pushd .
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
+
+# alternate location for boost
+if [ -d /usr/local/boost ]; then
+    export CMAKE_LIBRARY_PATH="/usr/local/boost/lib:$CMAKE_LIBRARY_PATH"
+    export CMAKE_INCLUDE_PATH="/usr/local/boost/include:$CMAKE_INCLUDE_PATH"
+fi
+
+# boost and cmake is FUBAR on RHEL6
+# passing -DBoost_NO_BOOST_CMAKE=ON works around this
+cmake ../$SOURCE_DIR -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_BUILD_TYPE=Debug && make
+popd
---
+++ scripts/install-nims.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+BUILD_DIR=../../nims-build
+SOURCE_DIR=../nims-source
+
+# this is here for embedded systems work, where the
+# IP address is used for multiple hosts
+SSH_OPTS='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+
+HOST=nims1
+USER=nims
+PASS=$1
+
+if [ "$1" = "" ] ; then
+    echo "password for $USER is required as argument" >&2
+    exit 1
+fi
+
+if ! ping -c 1 $HOST > /dev/null ; then
+    echo "change IP or connect host"
+    exit 1
+fi
+
+# set up after we get the IP right...
+SSH="sshpass -p $PASS ssh $SSH_OPTS $USER@$HOST"
+SCP="sshpass -p $PASS scp $SSH_OPTS"
+
+function copy_to_bin () {
+    
+    $SCP "$1" $USER@$HOST:~/bin
+    if ! [ $? = 0 ]; then
+        echo "failed to copy $1 to $HOST"
+        exit 1
+    fi
+}
+
+$SSH 'mkdir -p ~/bin'
+if ! [ $? = 0 ]; then
+    echo "failed to create ~/bin on $HOST"
+    exit 1
+fi
+
+copy_to_bin nims-build/config.yaml
+copy_to_bin nims-build/nims
+copy_to_bin nims-build/detector
+copy_to_bin nims-build/ingester
+copy_to_bin nims-build/tracker
