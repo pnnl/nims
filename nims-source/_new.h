ee7bdd151c10999239ef899f2cd04671110d03a8
d3x389 <d3x389@e0feaedf-03d4-4201-b3fb-2d10bec3ea31>
2015-05-19 18:38:28 +0000

    initial revision
    git-svn-id: https://subversion.pnnl.gov/svn/NIMS/trunk@46 e0feaedf-03d4-4201-b3fb-2d10bec3ea31

---
+++ nims-source/_new.h
@@ -0,0 +1,15 @@
+/*
+ *  Nekton Interaction Monitoring System (NIMS)
+ *
+ *  filename.h
+ *  
+ *  Created by Firstname Lastname on mm/dd/yyyy.
+ *  Copyright 2015 Pacific Northwest National Laboratory. All rights reserved.
+ *
+ */
+
+#ifndef __NIMS_FILENAME_H__
+#define __NIMS_FILENAME_H__
+
+
+#endif // __NIMS_FILENAME_H__
