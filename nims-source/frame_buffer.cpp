5f241e2e9cf4c20a725024d91232dbcaa3e80dc0
d3x389 <d3x389@e0feaedf-03d4-4201-b3fb-2d10bec3ea31>
2015-06-05 21:42:50 +0000

    implemented deletion of old shared frames, timed send to avoid blocking
    git-svn-id: https://subversion.pnnl.gov/svn/NIMS/trunk@60 e0feaedf-03d4-4201-b3fb-2d10bec3ea31

--- nims-source/frame_buffer.cpp
+++ nims-source/frame_buffer.cpp
@@ -108,29 +108,31 @@ FrameBufferWriter::FrameBufferWriter(const std::string &fb_name)
     mqw_name_ = "/nims_" + fb_name_ + "-connect";
     mqw_ = -1;
    
-   clog << "max messsage size is " << kMaxMessageSize << endl;
+    clog << "max messsage size is " << kMaxMessageSize << endl;
    
     // There must be one and only one instance of a writer for 
     // a FrameBufferInterface with a particular name.
-        // Create the message queue for receiving reader messages.
-        struct mq_attr attr;
-        attr.mq_flags = 0; // block on mq_send and mq_receive
-        attr.mq_maxmsg = 8;
-        attr.mq_msgsize = kMaxMessageSize;
-        
-        clog << "creating frame buffer connection msg queue " << mqw_name_ << endl;
-        // NOTE:  writer queue needs to be read/write so parent can send exit message to thread
-        mqw_ = mq_open(mqw_name_.c_str(),O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, &attr);
-        if (mqw_ == -1) 
-        {
-            BadWriterQueue e;
-            throw e;
-            return;
-        }
-        
-        // Start thread for servicing reader connections
-        clog << "starting connection thread" << endl;
-        t_ = std::thread(&FrameBufferWriter::HandleMessages, this);
+    // Create the message queue for receiving reader messages.
+    struct mq_attr attr;
+    attr.mq_flags = 0; // block on mq_send and mq_receive
+    attr.mq_maxmsg = 8;
+    attr.mq_msgsize = NAME_MAX;
+    
+    clog << "creating frame buffer connection msg queue " << mqw_name_ << endl;
+    // NOTE:  writer queue needs to be read/write so parent can send exit message to thread
+    mqw_ = mq_open(mqw_name_.c_str(),O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, &attr);
+    if (mqw_ == -1) 
+    {
+        perror("FrameBufferWriter");
+        BadWriterQueue e;
+        throw e;
+        return;
+    }
+    frame_count_ = 0;
+    
+    // Start thread for servicing reader connections
+    clog << "starting connection thread" << endl;
+    t_ = std::thread(&FrameBufferWriter::HandleMessages, this);
     
 } // FrameBufferWriter Constructor
 
@@ -154,6 +156,7 @@ FrameBufferWriter::~FrameBufferWriter()
     for (int k=0; k<mq_readers_.size(); ++k) mq_close(mq_readers_[k]);
     
     // clean up shared memory
+    for (int k=0; k<kMaxFramesInBuffer; ++k) shm_unlink(shm_names_[k].c_str());
     
 } // FrameBufferWriter Destructor
 
@@ -163,10 +166,10 @@ FrameBufferWriter::~FrameBufferWriter()
 // index of the new frame.
 long FrameBufferWriter::PutNewFrame(const Frame &new_frame)
 {
-    static int64_t framebuffer_frame_count = 0;
+    //static int64_t framebuffer_frame_count = 0; // made this a member var
     
     std::string shared_name(shm_prefix_);
-    shared_name += boost::lexical_cast<std::string>(framebuffer_frame_count++);
+    shared_name += boost::lexical_cast<std::string>(frame_count_++);
     
     /*
      Create -rw------- since we don't need executable pages.
@@ -211,7 +214,8 @@ long FrameBufferWriter::PutNewFrame(const Frame &new_frame)
     memcpy(shared_frame, &(new_frame.header), sizeof(new_frame.header));
     size_t data_size = new_frame.size();
     memcpy(shared_frame + sizeof(new_frame.header), &data_size, sizeof(data_size));
-    memcpy(shared_frame + sizeof(new_frame.header) + sizeof(data_size), new_frame.data_ptr(), data_size);
+    memcpy(shared_frame + sizeof(new_frame.header) + sizeof(data_size), 
+           new_frame.data_ptr(), data_size);
      
     // done with the region in this process; ok to do this?
     // pretty sure we can't shm_unlink here
@@ -220,18 +224,29 @@ long FrameBufferWriter::PutNewFrame(const Frame &new_frame)
         
     // create a message to notify the consumers
     cout << "sending frame messages to " << mq_readers_.size() << " readers" << endl;
-    FrameMsg msg(framebuffer_frame_count, map_length, shared_name);
+    FrameMsg msg(frame_count_, map_length, shared_name);
+    struct timespec tm;
+    clock_gettime(CLOCK_REALTIME, &tm); // get the current time
     for (int k=0; k<mq_readers_.size(); ++k)
     {
-        if (0 != mq_send(mq_readers_[k], (char *)(&msg), sizeof(msg), 0)) // priority 0
+        // TODO:  if a reader queue is full, then mq_send blocks, so one slow reader
+        //        could hold up other processes.  Could set O_NONBLOCK flag...
+        // O_NONBLOCK is not good because we wantreaders to block if queue is empty.
+        // If queue is full and time is past, then we'll return immediately.
+        if (0 != mq_timedsend(mq_readers_[k], (char *)(&msg), sizeof(msg), 0, &tm)) 
         {
             // TODO:  Need to handle an error here more comprehensively. 
-            //        Check errno and respond accordingly.
+            //        If there is problem with queue, may need to remove it from the list.
             perror("mq_send() in FrameBufferInterface::PutNewFrame");
         }
     } // for mq_readers_
+    
+    // unlink oldest shared frame and save the name of new frame
+    int ind = frame_count_ % kMaxFramesInBuffer;
+    shm_unlink(shm_names_[ind].c_str());
+    shm_names_[ind] = shared_name;
 
-    return framebuffer_frame_count; 
+    return frame_count_;
     
 } // FrameBufferWriter::PutNewFrame
 	    
@@ -282,12 +297,13 @@ FrameBufferReader::FrameBufferReader(const std::string &fb_name)
         mqr_name_ = "/nims_" + fb_name_ + "-mq-" + boost::lexical_cast<std::string>(getpid());
         struct mq_attr attr;
         attr.mq_flags = 0;
-        attr.mq_maxmsg = 8; //TODO:  Define max number of buffered frames
+        attr.mq_maxmsg = 10; // system limit in /proc/sys/fs/mqueue/msg_max
         attr.mq_msgsize = kMaxMessageSize;
         clog << "creating reader message queue " << mqr_name_ << endl;
         mqr_ = mq_open(mqr_name_.c_str(),O_RDONLY | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, &attr);
         if (mqr_ == -1) 
         {
+            perror("FrameBufferReader");
             BadReaderQueue e;
             throw e;
             return;
@@ -327,7 +343,7 @@ FrameBufferReader::FrameBufferReader(const std::string &fb_name)
 FrameBufferReader::~FrameBufferReader()
 {
         mq_close(mqr_);
-        mq_unlink(mqr_name_.c_str()); // TODO: make sure this doesn't block
+        mq_unlink(mqr_name_.c_str());
 
     // clean up shared memory
     
@@ -336,25 +352,37 @@ FrameBufferReader::~FrameBufferReader()
 //-----------------------------------------------------------------------------
 // Get the next frame in the buffer, "next" meaning
 // relative to the last frame that was retrieved by the
-// calling process.  Returns the index of the frame.
+// calling process.  Returns the index of the frame if successful.
 long FrameBufferReader::GetNextFrame(Frame* next_frame)
 {
-    // Note this will block until there's a message from the writer.
-    FrameMsg msg;
-    mq_receive(mqr_, (char *)&msg, sizeof(msg), 0);
-    cout << "getting " << msg.shm_open_name << endl;
-    int fd = shm_open(msg.shm_open_name, O_RDONLY, S_IRUSR);
-    
-    // !!! early return
-    if (-1 == fd) {
-        perror("shm_open() in FrameBufferInterface::GetNextFrame");
+    // TODO:  Not sure if this is a valid test to make sure the caller
+    //        is passing the address of a Frame struct.
+    if (next_frame == nullptr)
+    {
+        cerr << "GetNextFrame: pointer argument must be initialized!" << endl;
         return -1;
     }
+    // Read messages until we get a valid shared memory name.  If the reader
+    // is behind, then a message may be old and the shared memory name 
+    // contained in the message may already be unlinked.
+    FrameMsg msg;
+     int fd = -1; // shared memory file descriptor
+     while ( fd == -1 )
+    {
+        // Note this will block if queue is empty.
+        if ( -1 == mq_receive(mqr_, (char *)&msg, sizeof(msg), 0) )
+        {
+            perror("GetNextFrame");
+            return -1;
+        }
+        // Attempt to open the shared memory.
+        fd = shm_open(msg.shm_open_name, O_RDONLY, S_IRUSR);
+   }
     
+    cout << "GetNextFrame: getting " << msg.shm_open_name << endl;
     // size of mmap region
-    //assert(msg.mapped_data_length > sizeof(Frame));
-    //clog << "expected mmap data name: " << msg.shm_open_name << endl;
-    //clog << "expected mmap data size: " << msg.mapped_data_length << endl;
+    assert(msg.mapped_data_size > sizeof(Frame));
+    clog << "GetNextFrame: data size = " << msg.mapped_data_size << endl;
     
     // mmap a shared framebuffer on the file descriptor we have from shm_open
     char *shared_frame;
@@ -365,8 +393,7 @@ long FrameBufferReader::GetNextFrame(Frame* next_frame)
     
     // !!! early return
     if (MAP_FAILED == shared_frame) {
-        perror("mmap() in FrameBufferInterface::GetNextFrame");
-        shm_unlink(msg.shm_open_name);
+        perror("mmap() in GetNextFrame");
         return -1;
     }
     
@@ -381,12 +408,14 @@ long FrameBufferReader::GetNextFrame(Frame* next_frame)
         cerr << "Error: Can't allocate memory for frame data." << endl;
         return -1;
     }
-    memcpy(next_frame->data_ptr(), shared_frame + sizeof(next_frame->header) + sizeof(data_size), data_size);
+    memcpy(next_frame->data_ptr(), shared_frame + sizeof(next_frame->header) 
+           + sizeof(data_size), data_size);
     
     // clean up
+    //clog << "GetNextFrame: unmapping shared memory" << endl;
     munmap(shared_frame, msg.mapped_data_size);
-    shm_unlink(msg.shm_open_name);
     
+    //clog << "GetNextFrame: Done." << endl;
     return msg.frame_number;
     
 } // FrameBufferReader::GetNextFrame
