4be5e8a98924c6bec48a173739781a0126ec0aa8
d3x389 <d3x389@e0feaedf-03d4-4201-b3fb-2d10bec3ea31>
2015-06-24 23:24:40 +0000

    added config params for tracker, new frame buffer interface to tracker
    git-svn-id: https://subversion.pnnl.gov/svn/NIMS/trunk@100 e0feaedf-03d4-4201-b3fb-2d10bec3ea31

--- nims-source/tracker.cpp
+++ nims-source/tracker.cpp
@@ -39,7 +39,7 @@ int main (int argc, char * argv[]) {
 	po::options_description desc;
 	desc.add_options()
 	("help",                                                              "print help message")
-	("cfg,c", po::value<string>()->default_value( "./nims-config.yaml" ), "path to config file; default is ./nims-config.yaml")
+	("cfg,c", po::value<string>()->default_value( "./config.yaml" ), "path to config file; default is ./config.yaml")
 	;
 	po::variables_map options;
     try
@@ -60,71 +60,88 @@ int main (int argc, char * argv[]) {
         cerr << desc << endl;
         return 0;
     }
-	
-    // TODO:  validate config path
-    fs::path cfgfilepath( options["cfg"].as<string>() );
-    if ( ! (fs::exists(cfgfilepath) && fs::is_regular_file(cfgfilepath)) )
-    {
-        cerr << "Can't find config file " << cfgfilepath.string() << endl;
-        return -1;
-    }
-    YAML::Node config = YAML::LoadFile(cfgfilepath.string()); // throws exception if bad path
-    
-    // TODO: read parameters from config file
-    string buffer_name = "nims";
-    fs::path avifilepath("tracks.avi");
-    int N = 100; // number of pings used for moving average
     
 	//--------------------------------------------------------------------------
 	// DO STUFF
 	cout << endl << "Starting " << argv[0] << endl;
-    //SubprocessCheckin(getpid()); // synchronize with main NIMS process
-	/*
-    VideoWriter outvideo(avifilepath.string().c_str(), CV_FOURCC('D','I','V','X'), 10,
+	
+	string fb_name;
+	int N;
+    fs::path cfgfilepath( options["cfg"].as<string>() );
+    try 
+    {
+        YAML::Node config = YAML::LoadFile(cfgfilepath.string());
+         fb_name = config["FRAMEBUFFER_NAME"].as<string>();
+         N = config["NUM_PINGS_FOR_MOVING_AVG"].as<int>();
+     }
+     catch( const std::exception& e )
+    {
+        cerr << "Error reading config file." << e.what() << endl;
+        cerr << desc << endl;
+        return -1;
+    }
+    
+    
+	
+ 	/*
+     fs::path avifilepath("tracks.avi");
+   VideoWriter outvideo(avifilepath.string().c_str(), CV_FOURCC('D','I','V','X'), 10,
 						 Size(DUALFRAME ? 2*info.ixsize : info.ixsize, info.iysize));
 	if ( !outvideo.isOpened() )
 	{
 		cerr << "Could not open output video file " << avifilepath.string() << endl;
 		return -1;
 	}*/
+    //-------------------------------------------------------------------------
+	// SETUP TRACKING
+    
+	vector<TrackedObject> active_tracks;   // tracks still being updated
+	vector<int> active_id;                 // unique id for each track
+	vector<TrackedObject> completed_tracks;// completed tracks (no updates)
+	vector<int> completed_id;              // unique id
+	int next_id = 0;
+	
+	// NOTE:  The noise values are somewhat arbitrary.  The important 
+	//        thing is their relative values. Here, the process noise
+	//         should be much greater than the measurement noise because
+	//        an animals's progress through the 3D space of moving water 
+	//        is very noisy.  The measurement noise is the resolution
+	//        of the sample space (range bin, beam width).
+	// TODO:  make these params in config file
+    float process_noise = 1e-1;     // process is fish swimming
+    float measurement_noise = 1e-2; // measurement is backscatter
+	int pred_err_max = 15; // max difference in pixels between prediction and actual
+
+    clog << endl << "tracking with process noise = " << process_noise 
+         << " and measurement noise = " << measurement_noise << endl;
+
+
     Mat_<framedata_t> ping_mean; // moving average echo intensity (background)
     Mat_<framedata_t> ping_stdv; // moving std dev of echo intensity (background)
     
-    //--------------------------------------------------------------------------
-	// SETUP TRACKING
-    
-	vector<TrackedObject> active_tracks;   // objects that are actively being tracked
-	vector<int> active_id; // unique id for each tracked object
-	vector<TrackedObject> completed_tracks; // objects that have permanently left the field of view
-	vector<int> completed_id; // unique id
-	int next_id = 0;
-	// NOTE:  The noise values are somewhat arbitrary.  The important thing is their relative values;
-	//        in this case, the process noise should be much greater than the measurement noise because
-	//        a juvenile fish's progress through the 3D space of moving water is very noisy.  The measurement noise
-	//        is the resolution of the sample space (range bin, beam width).
-    float process_noise = 1e-1;     // process is fish swimming
-    float measurement_noise = 1e-2; // measurement is detection in range-beam space
-	// TODO:  Set max prediction error based on pixel size/fish size? noise?
-	int pred_err_max = 15; // max distance in pixels between prediction and actual
-
-    clog << endl << "tracking with process noise = " << process_noise << " and measurement noise = " << measurement_noise << endl;
-
-
-    try
-    {
-        FrameBufferReader fb(buffer_name, config["FB_WRITER_QUEUE"].as<string>());
-        Frame next_ping;
+        FrameBufferReader fb(fb_name);
+        if ( -1 == fb.Connect() )
+        {
+            cerr << argv[0] << "Error connecting to framebuffer." << endl;
+            return -1;
+        }
         
         // Initialize the mean and std dev of the echo intensity.
         // Get one ping to get the dimensions of the ping data.
-        if ( fb.GetNextFrame(&next_ping)==-1 )
+         Frame next_ping;
+        
+       if ( fb.GetNextFrame(&next_ping)==-1 )
         {
             cerr << argv[0] << "Error getting ping to initialize moving average." << endl;
             return -1;
         }
 
-        int dim_sizes[] = {(int)next_ping.header.num_samples, (int)next_ping.header.num_beams};
-        int cv_type = CV_64FC1; // assuming double, TODO:  Need to get this dynamically from framedata_t
+    SubprocessCheckin(getpid()); // synchronize with main NIMS process
+    
+       int dim_sizes[] = {(int)next_ping.header.num_samples, 
+                           (int)next_ping.header.num_beams};
+        int cv_type = sizeof(framedata_t)==32 ? CV_32FC1 : CV_64FC1; 
+        
         size_t total_samples = next_ping.header.num_samples*next_ping.header.num_beams;
         Mat_<framedata_t> pings(N, total_samples);
         
@@ -133,7 +150,8 @@ int main (int argc, char * argv[]) {
         {
             if ( fb.GetNextFrame(&next_ping)==-1 )
             {
-                cerr << argv[0] << "Error getting ping to initialize moving average." << endl;
+                cerr << argv[0] 
+                     << "Error getting ping to initialize moving average." << endl;
                 return -1;
             }
             // Create a cv::Mat wrapper for the ping data
@@ -141,7 +159,7 @@ int main (int argc, char * argv[]) {
             ping_data.reshape(0,1).copyTo(pings.rowRange(k,k+1));
         }
         meanStdDev(pings, ping_mean, ping_stdv);
-        clog << "initialized" << endl;
+        clog << "moving average and std dev initialized" << endl;
         
         int frame_index = -1;
         while ( (frame_index = fb.GetNextFrame(&next_ping)) != -1)
@@ -153,12 +171,14 @@ int main (int argc, char * argv[]) {
             // update background
             clog << "updating mean background"  << endl;
             Mat new_mean = ping_mean + (ping_data.reshape(0,1) - ping_mean)/N;
-            ping_stdv = ping_stdv + (ping_data.reshape(0,1) - ping_mean).mul(ping_data.reshape(0,1) - new_mean);
+            ping_stdv = ping_stdv + 
+                (ping_data.reshape(0,1) - ping_mean).mul(ping_data.reshape(0,1) - new_mean);
             ping_mean = new_mean;
             
             // detect targets
             clog << "detecting targets" << endl;
-            Mat foregroundMask = (ping_data.reshape(0,1) - ping_mean) / ping_stdv > 3; // TODO: Make this a parameter
+            // TODO: Make number of std devs a parameter
+            Mat foregroundMask = (ping_data.reshape(0,1) - ping_mean) / ping_stdv > 3; 
             int nz = countNonZero(foregroundMask);
             if (nz > 0)
             {
@@ -173,12 +193,7 @@ int main (int argc, char * argv[]) {
             clog << "tracking" << endl;
             
         }
-    }
-	catch( const std::exception& e )
-	{
-        cerr << argv[0] << ":  " << e.what() << endl;
-	}
-    
+     
 	cout << endl << "Ending " << argv[0] << endl << endl;
     return 0;
 }
