a5c1bbcbeba98ddf9b988335968292f5871391d1
d3x389 <d3x389@e0feaedf-03d4-4201-b3fb-2d10bec3ea31>
2015-06-24 18:20:51 +0000

    test scripts for tcp/ip connectivity
    git-svn-id: https://subversion.pnnl.gov/svn/NIMS/trunk@94 e0feaedf-03d4-4201-b3fb-2d10bec3ea31

---
+++ scripts/client.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python 
+
+""" 
+A simple echo server 
+""" 
+
+import socket 
+from time import sleep
+
+#host = '127.0.0.1' 
+host = 'we18320' 
+port = 20001 
+backlog = 5 
+size = 4096
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.connect((host, port))
+
+def hex_to_string(value):
+    return value
+    return "".join([chr(x) for x in value])
+
+data = ""
+
+while 1: 
+    data += s.recv(5)
+    #print hex_to_string(data)
+    print data
