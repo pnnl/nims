706b28f021be3399fe7b971f8c52c720262b7a72
d3m570 <d3m570@e0feaedf-03d4-4201-b3fb-2d10bec3ea31>
2015-07-14 19:31:48 +0000

    - Check in with nims process earlier, as getting the first ping   takes a long time (sometimes?). - Revise logging to remove extra newlines, since the boost log API   implicitly adds and endl to each log.
    git-svn-id: https://subversion.pnnl.gov/svn/NIMS/trunk@129 e0feaedf-03d4-4201-b3fb-2d10bec3ea31

--- nims-source/tracker.cpp
+++ nims-source/tracker.cpp
@@ -39,7 +39,7 @@ int PingImagePolarToCart(const FrameHeader &hdr, OutputArray _map_x, OutputArray
 {
     // range bin resolution in meters per pixel
     float rng_step = (hdr.range_max_m - hdr.range_min_m)/(hdr.num_samples - 1);
-    NIMS_LOG_DEBUG << "PingImagePolarToCart: range resolution is " << rng_step << endl;
+    NIMS_LOG_DEBUG << "PingImagePolarToCart: range resolution is " << rng_step;
     float y2 = hdr.range_max_m; // y coordinate of first row of image pixels
     
     float beam_step = hdr.beam_angles_deg[1] - hdr.beam_angles_deg[0];
@@ -48,14 +48,14 @@ int PingImagePolarToCart(const FrameHeader &hdr, OutputArray _map_x, OutputArray
     float x1 = hdr.range_max_m*sin(theta1);
     float x2 = hdr.range_max_m*sin(theta2);
     NIMS_LOG_DEBUG << "PingImagePolarToCart: beam angles from " << hdr.beam_angles_deg[0]
-         << " to " << hdr.beam_angles_deg[hdr.num_beams-1] << endl;
-    NIMS_LOG_DEBUG << "PingImagePolarToCart: beam angle step is " << beam_step << endl;
-    NIMS_LOG_DEBUG << "PingImagePolarToCart: x1 = " << x1 << ", x2 = " << x2 << endl;
+         << " to " << hdr.beam_angles_deg[hdr.num_beams-1];
+    NIMS_LOG_DEBUG << "PingImagePolarToCart: beam angle step is " << beam_step;
+    NIMS_LOG_DEBUG << "PingImagePolarToCart: x1 = " << x1 << ", x2 = " << x2;
 
     // size of image
     int nrows = hdr.num_samples;
     int ncols = (x2 - x1) / rng_step;
-    NIMS_LOG_DEBUG << "PingImagePolarToCart: image size is " << nrows << " x " << ncols << endl;
+    NIMS_LOG_DEBUG << "PingImagePolarToCart: image size is " << nrows << " x " << ncols;
     
     _map_x.create(nrows, ncols, CV_32FC1);
     Mat_<float> map_x = _map_x.getMat();
@@ -141,8 +141,8 @@ int main (int argc, char * argv[]) {
     }
     catch( const std::exception& e )
     {
-        NIMS_LOG_ERROR << "Sorry, couldn't parse that: " << e.what() << endl;
-        NIMS_LOG_ERROR << desc << endl;
+        NIMS_LOG_ERROR << "Sorry, couldn't parse that: " << e.what();
+        NIMS_LOG_ERROR << desc;
         return -1;
     }
 	
@@ -150,7 +150,7 @@ int main (int argc, char * argv[]) {
 	
     if( options.count( "help" ) > 0 )
     {
-        NIMS_LOG_ERROR << desc << endl;
+        NIMS_LOG_ERROR << desc;
         return 0;
     }
     
@@ -183,8 +183,8 @@ int main (int argc, char * argv[]) {
      }
      catch( const std::exception& e )
     {
-        NIMS_LOG_ERROR << "Error reading config file." << e.what() << endl;
-        NIMS_LOG_ERROR << desc << endl;
+        NIMS_LOG_ERROR << "Error reading config file." << e.what();
+        NIMS_LOG_ERROR << desc;
         return -1;
     }
     
@@ -196,7 +196,7 @@ int main (int argc, char * argv[]) {
 						 Size(DUALFRAME ? 2*info.ixsize : info.ixsize, info.iysize));
 	if ( !outvideo.isOpened() )
 	{
-		NIMS_LOG_ERROR << "Could not open output video file " << avifilepath.string() << endl;
+		NIMS_LOG_ERROR << "Could not open output video file " << avifilepath.string();
 		return -1;
 	}*/
     //-------------------------------------------------------------------------
@@ -220,26 +220,29 @@ int main (int argc, char * argv[]) {
 	int pred_err_max = 15; // max difference in pixels between prediction and actual
 
     NIMS_LOG_DEBUG << "tracking with process noise = " << process_noise 
-                   << " and measurement noise = " << measurement_noise << endl;
+                   << " and measurement noise = " << measurement_noise;
 
 
         FrameBufferReader fb(fb_name);
         if ( -1 == fb.Connect() )
         {
-            NIMS_LOG_ERROR << "Error connecting to framebuffer." << endl;
+            NIMS_LOG_ERROR << "Error connecting to framebuffer.";
             return -1;
         }
-        
+
+        // check in before calling GetNextFrame, to avoid timeout in the
+        // NIMS parent process (and subsequent termination).
+        // FIXME: figure out why it takes so long to get the first ping
+        SubprocessCheckin(getpid());
+
         // Get one ping to get the dimensions of the ping data.
          Frame next_ping;
         
        if ( fb.GetNextFrame(&next_ping)==-1 )
         {
-            NIMS_LOG_ERROR << "Error getting first ping for moving average." << endl;
+            NIMS_LOG_ERROR << "Error getting first ping for moving average.";
             return -1;
         }
-
-        SubprocessCheckin(getpid()); // synchronize with main NIMS process
     
         // NOTE:  data is stored transposed
         int dim_sizes[] = {(int)next_ping.header.num_beams, 
@@ -250,9 +253,9 @@ int main (int argc, char * argv[]) {
         size_t total_samples = next_ping.header.num_samples*next_ping.header.num_beams;
         
         NIMS_LOG_DEBUG << "   num samples = " << dim_sizes[0] 
-                       << ", num beams = " << dim_sizes[1] << endl;
-        NIMS_LOG_DEBUG << "   bytes per sample = " << sizeof(framedata_t) << endl;
-        NIMS_LOG_DEBUG << "   total samples = " << total_samples << endl;
+                       << ", num beams = " << dim_sizes[1];
+        NIMS_LOG_DEBUG << "   bytes per sample = " << sizeof(framedata_t);
+        NIMS_LOG_DEBUG << "   total samples = " << total_samples;
         
         Mat map_x;
         Mat map_y;
@@ -264,12 +267,12 @@ int main (int argc, char * argv[]) {
             minMaxIdx(map_y.reshape(0,1), &min_rng, &max_rng);
             
             NIMS_LOG_DEBUG << "image mapping:  beam index is from " 
-                           << min_beam << " to " << max_beam << endl;
+                           << min_beam << " to " << max_beam;
             NIMS_LOG_DEBUG << "image mapping:  range index is from " 
-                           << min_rng << " to " << max_rng << endl;
+                           << min_rng << " to " << max_rng;
         }
         
-        NIMS_LOG_DEBUG << "initializing moving average and std dev" << endl;
+        NIMS_LOG_DEBUG << "initializing moving average and std dev";
        // TODO:  would be good to free this when done intitializing
        Mat_<framedata_t> pings(N, total_samples);  
        
@@ -280,7 +283,7 @@ int main (int argc, char * argv[]) {
             if ( fb.GetNextFrame(&next_ping)==-1 )
             {
                 NIMS_LOG_ERROR << argv[0] 
-                     << "Error getting ping for initial moving average." << endl;
+                     << "Error getting ping for initial moving average.";
                 return -1;
             }
             // Create a cv::Mat wrapper for the ping data
@@ -293,11 +296,11 @@ int main (int argc, char * argv[]) {
         double min_val, max_val;
         minMaxIdx(pings.row(N-1), &min_val, &max_val);
         NIMS_LOG_DEBUG << "ping data values from " << min_val 
-                       << " to " << max_val << endl;
+                       << " to " << max_val;
         // NOTE:  Values in Dolphin(0) are from 0 to 13.0686
         minMaxIdx(ping_mean.reshape(0,1), &min_val, &max_val);
         NIMS_LOG_DEBUG << "mean background values from " << min_val 
-                       << " to " << max_val << endl;
+                       << " to " << max_val;
        // Initialize the moving std dev. 
        Mat ping_stdv = Mat::zeros(dim_sizes[0],dim_sizes[1],cv_type); 
         for (int k=0; k<N; ++k)
@@ -310,19 +313,18 @@ int main (int argc, char * argv[]) {
  
          minMaxIdx(ping_stdv.reshape(0,1), &min_val, &max_val);
         NIMS_LOG_DEBUG << "background std. dev. values from " << min_val 
-                       << " to " << max_val << endl;
+                       << " to " << max_val;
   
-        NIMS_LOG_DEBUG << "moving average and std dev initialized" << endl;
+        NIMS_LOG_DEBUG << "moving average and std dev initialized";
         
         int frame_index = -1;
         while ( (frame_index = fb.GetNextFrame(&next_ping)) != -1)
         {
-            NIMS_LOG_DEBUG << "got frame " << frame_index << endl;
-            NIMS_LOG_DEBUG << endl << next_ping.header << endl;
+            NIMS_LOG_DEBUG << "got frame " << frame_index << endl << next_ping.header;
             Mat ping_data(2,dim_sizes,cv_type,next_ping.data_ptr());
             
             // update background
-            NIMS_LOG_DEBUG << "updating mean background"  << endl;
+            NIMS_LOG_DEBUG << "updating mean background";
             // u(k) = u(k-1) + (x - u(k-1))/N
             Mat new_mean = ping_mean + (ping_data - ping_mean)/N;
             // s(k) = s(k-1) + (x - u(k-1))(x - u(k))/(N-1)
@@ -356,7 +358,7 @@ int main (int argc, char * argv[]) {
             }
              
             // detect targets
-            NIMS_LOG_DEBUG << "detecting targets" << endl;
+            NIMS_LOG_DEBUG << "detecting targets";
             // TODO: Make number of std devs a parameter
             Mat foregroundMask = (ping_data - ping_mean) / ping_stdv > 3; 
             int nz = countNonZero(foregroundMask);
@@ -365,15 +367,15 @@ int main (int argc, char * argv[]) {
                 PixelGrouping objects;
                 group_pixels(foregroundMask, objects);
                 int n_obj = objects.size();
-                cout << "found " << n_obj << " obj:" << endl;
+                cout << "found " << n_obj << " obj:";
 
             } // nz > 0
             
             // update tracks
-            NIMS_LOG_DEBUG << "tracking" << endl;
+            NIMS_LOG_DEBUG << "tracking";
             
         }
      
-	cout << endl << "Ending " << argv[0] << endl << endl;
+	cout << endl << "Ending " << argv[0] << endl;
     return 0;
 }
