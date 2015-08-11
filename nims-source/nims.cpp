ca4fe05b0d6b68ae4dbffd894b052a03cf5ac4f7
d3m570 <d3m570@e0feaedf-03d4-4201-b3fb-2d10bec3ea31>
2015-08-11 21:25:36 +0000

    Major changes to nims, to try and make restarting more robust. The motivation is to allow the nims process to restart the webapp after restarting all of its child processes, in order to force it to reestablish message queue and shared memory connections.
    - Refactor warm restart code
    - Implement a SIGHUP handler to call WarmRestart
    - Catch SIGTERM and treat it like SIGINT
    - Add code to restart webapp
    
    TODO: paths for the webapp need to be modified.
    
    
    git-svn-id: https://subversion.pnnl.gov/svn/NIMS/trunk@160 e0feaedf-03d4-4201-b3fb-2d10bec3ea31

--- nims-source/nims.cpp
+++ nims-source/nims.cpp
@@ -26,7 +26,7 @@
 #include "yaml-cpp/yaml.h"
 
 #include "queues.h"
-#include "task.h"//
+#include "task.h"
 #include "log.h"
 
 using namespace std;
@@ -36,11 +36,21 @@ namespace fs = boost::filesystem;
 
 static volatile sig_atomic_t sigint_received_ = 0;
 static volatile sig_atomic_t sigchld_received_ = 0;
+static volatile sig_atomic_t sighup_received_ = 0;
 static vector<nims::Task *> *child_tasks_ = NULL;
 
+#define HANDLE_EINTR(x) ({ \
+    __typeof__(x) __eintr_result__; \
+    do { \
+        __eintr_result__ = x; \
+    } while (__eintr_result__ == -1 && errno == EINTR); \
+    __eintr_result__;\
+})
+
+// treat SIGINT and SIGTERM as identical
 static void sigint_handler(int sig)
 {
-    if (SIGINT == sig)
+    if (SIGINT == sig || SIGTERM == sig)
         sigint_received_ += 1;
 }
 
@@ -48,6 +58,12 @@ static void sigchld_handler(int sig)
 {
     if (SIGCHLD == sig)
         sigchld_received_ += 1;
+}
+
+static void sighup_handler(int sig)
+{
+    if (SIGHUP == sig)
+        sighup_received_ += 1;
 }
 
 static void InterruptChildProcesses()
@@ -129,6 +145,7 @@ static int WaitForTaskLaunch(nims::Task *task, const mqd_t mq)
     
 }
 
+// exit() on failure
 static void LaunchProcessesFromConfig(const YAML::Node &config)
 {
     fs::path bin_dir(config["BINARY_DIR"].as<string>());
@@ -177,6 +194,64 @@ static void LaunchProcessesFromConfig(const YAML::Node &config)
     mq_unlink(MQ_SUBPROCESS_CHECKIN_QUEUE);
 }
 
+// exit() on failure
+static void WarmRestart(std::string &cfgpath)
+{
+    InterruptChildProcesses();
+    
+    // reload config file; mainly for HUP, but ok for CHLD
+    YAML::Node config = YAML::LoadFile(cfgpath);
+    LaunchProcessesFromConfig(config);
+    
+    // ignore SIGCHLD while restarting nims.py
+    struct sigaction new_action, old_action;
+    sigemptyset(&new_action.sa_mask);
+    new_action.sa_flags = 0;
+
+    new_action.sa_handler = SIG_DFL;
+    sigaction(SIGCHLD, NULL, &old_action);
+    if (SIG_DFL != old_action.sa_handler)
+        sigaction(SIGCHLD, &new_action, NULL);
+    
+    const char * stop_args[] = {"--stop", "--oknodo", "--name", "nims.py"};
+    vector<string> args(stop_args, stop_args + sizeof(stop_args) / sizeof(char *));
+    nims::Task task = nims::Task("/sbin/start-stop-daemon", args);
+    
+    int ret, status;
+    if (task.launch()) {
+        ret = HANDLE_EINTR(waitpid(task.get_pid(), &status, 0));
+    }
+    if (-1 != ret && WIFEXITED(status)) {
+        NIMS_LOG_DEBUG << "nims.py exited; start-stop-daemon status " << WEXITSTATUS(status);
+    } else {
+        // NB: start-stop-daemon --oknodo returns 0 if no matching process
+        nims_perror("start-stop-daemon failed to stop nims.py");
+        NIMS_LOG_ERROR << "failed to stop nims.py";
+        NIMS_LOG_ERROR << "ret = " << ret << ", WIFEXITED(status) = " << WIFEXITED(status);
+        exit(errno);
+    }
+    
+    const char *start_args[] = {"--start", "--background",
+        "--chdir", "../webapp", "--exec", "../webapp/nims.py"};
+    args = std::vector<string>(start_args, start_args + sizeof(start_args) / sizeof(char *));
+    task = nims::Task("/sbin/start-stop-daemon", args);
+    ret, status;
+    if (task.launch()) {
+        ret = HANDLE_EINTR(waitpid(task.get_pid(), &status, 0));
+    }
+    if (-1 != ret && WIFEXITED(status)) {
+        NIMS_LOG_DEBUG << "nims.py started with status " << WEXITSTATUS(status);
+    } else {
+        nims_perror("start-stop-daemon failed to start nims.py");
+        NIMS_LOG_ERROR << "failed to start nims.py";
+        NIMS_LOG_ERROR << "ret = " << ret << ", WIFEXITED(status) = " << WIFEXITED(status);
+        exit(errno);
+    }
+    
+    // restore SIGCHLD handler
+    sigaction(SIGCHLD, &old_action, NULL);
+}
+
 int main (int argc, char * argv[]) {
 	//--------------------------------------------------------------------------
     // PARSE COMMAND LINE
@@ -187,7 +262,6 @@ int main (int argc, char * argv[]) {
     ("cfg,c", po::value<string>()->default_value("config.yaml"),         "path to config file")
     ("log,l", po::value<string>()->default_value("warning"), "debug|warning|error")
     ("daemon,D", "run in background")
-	//("bar,b",   po::value<unsigned int>()->default_value( 101 ),"an integer value")
 	;
 	po::variables_map options;
     try
@@ -232,15 +306,18 @@ int main (int argc, char * argv[]) {
 
     // register for SIGINT
     new_action.sa_handler = sigint_handler;
-    sigaction(SIGINT, NULL, &old_action);
-    if (SIG_IGN != old_action.sa_handler)
-        sigaction(SIGINT, &new_action, NULL);
+    sigaction(SIGINT, &new_action, NULL);
+
+    // register for SIGTERM, same action as SIGINT
+    sigaction(SIGTERM, &new_action, NULL);
     
     // register for SIGCHLD
     new_action.sa_handler = sigchld_handler;
-    sigaction(SIGCHLD, NULL, &old_action);
-    if (SIG_IGN != old_action.sa_handler)
-        sigaction(SIGCHLD, &new_action, NULL);
+    sigaction(SIGCHLD, &new_action, NULL);
+
+    // register for SIGHUP
+    new_action.sa_handler = sighup_handler;
+    sigaction(SIGHUP, &new_action, NULL);
     
     // ignore SIGPIPE
     new_action.sa_handler = SIG_IGN;
@@ -282,6 +359,9 @@ int main (int argc, char * argv[]) {
         nims_perror("epoll_ctl() failed");
         exit(2);
     }
+    
+    // reset before entering the event loop
+    sigchld_received_ = 0;
 
     while (true) {
         
@@ -298,40 +378,53 @@ int main (int argc, char * argv[]) {
         // handle error condition first, in case we're exiting on a signal
         if (-1 == nfds) {
             
-            if (EINTR == errno) {
-            
-                // atexit will call InterruptChildProcesses
-                if (sigint_received_) {
-                    NIMS_LOG_WARNING << "caught SIGINT";            
-                    break;
-                }
-                else if (sigchld_received_) {
-                    NIMS_LOG_ERROR << "caught SIGCHLD";
-                    static time_t last_relaunch_time_ = 0;
-                    time_t current_time = time(NULL);
-#define MIN_RELAUNCH_TIME_INTERVAL 60
-                    if ((current_time - last_relaunch_time_) > MIN_RELAUNCH_TIME_INTERVAL) {
-                        NIMS_LOG_ERROR << "attempting warm restart";
-                        InterruptChildProcesses();
-                        LaunchProcessesFromConfig(config);
-                        last_relaunch_time_ = current_time;
-                        NIMS_LOG_ERROR << "warm restart succeeded";
-                    }
-                    else {
-                        // stop and let atexit clean up
-                        NIMS_LOG_ERROR << "Last relaunch was less than " 
-                            << MIN_RELAUNCH_TIME_INTERVAL
-                            << " seconds ago. Exiting.";
-                        break;
-                    }
-                }
-                
-            } else if (EINTR != errno) {
-                // unhandled signal or some other error
+            if (EINTR != errno) {
+                // unhandled error
                 nims_perror("epoll_wait() failed");
                 exit(3);
             }
-        
+            
+            assert(EINTR == errno);
+                        
+            // atexit will call InterruptChildProcesses
+            if (sigint_received_) {
+                NIMS_LOG_WARNING << "caught SIGINT or SIGTERM";            
+                break;
+            }
+            else if (sigchld_received_) {
+                // reset, so we don't misinterpret HUP/CHLD
+                sigchld_received_ = 0;
+                NIMS_LOG_ERROR << "caught SIGCHLD";
+                static time_t last_relaunch_time_ = 0;
+                time_t current_time = time(NULL);
+#define MIN_RELAUNCH_TIME_INTERVAL 60
+                if ((current_time - last_relaunch_time_) > MIN_RELAUNCH_TIME_INTERVAL) {
+                    NIMS_LOG_ERROR << "attempting warm restart";
+                    WarmRestart(cfgpath);
+                    last_relaunch_time_ = current_time;
+                    NIMS_LOG_ERROR << "warm restart succeeded";
+                }
+                else {
+                    // stop and let atexit clean up
+                    NIMS_LOG_ERROR << "Last relaunch was less than " 
+                        << MIN_RELAUNCH_TIME_INTERVAL
+                        << " seconds ago. Exiting.";
+                    break;
+                }
+            }
+            else if (sighup_received_) {
+                // reset, so we don't misinterpret HUP/CHLD
+                sighup_received_ = 0;
+                NIMS_LOG_ERROR << "SIGHUP caught: attempting warm restart";
+                WarmRestart(cfgpath);
+                NIMS_LOG_ERROR << "SIGHUP warm restart succeeded";
+            }
+            else {
+                // this is a programmer error; die
+                NIMS_LOG_ERROR << "Unhandled signal";
+                break;
+            }
+
         }
         
     }
