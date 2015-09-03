6c3948a2e6a48304b56ab5bae1cbce3fcc6b1e1d
d3x389 <d3x389@e0feaedf-03d4-4201-b3fb-2d10bec3ea31>
2015-09-03 19:06:13 +0000

    initial revision
    git-svn-id: https://subversion.pnnl.gov/svn/NIMS/trunk@165 e0feaedf-03d4-4201-b3fb-2d10bec3ea31

---
+++ nims-source/detections.h
@@ -0,0 +1,47 @@
+/*
+ *  Nekton Interaction Monitoring System (NIMS)
+ *
+ *  detections.h
+ *  
+ *  Created by Shari Matzner on 08/20/2015.
+ *  Copyright 2015 Pacific Northwest National Laboratory. All rights reserved.
+ *
+ */
+
+#ifndef __NIMS_DETECTIONS_H__
+#define __NIMS_DETECTIONS_H__
+
+#include <cstdint>  // fixed width integer types
+
+#define MAX_DETECTIONS_PER_FRAME 100
+
+// Data structure for describing the objects detected
+// in one ping image.  Detections are possibly fish
+// or marine mammals or some other object moving
+// through the sonar field of view.
+struct Detection
+{
+    // location of the center of the detected object
+    float center_range;
+    float center_beam;
+    // unique id of track that detection is assigned to
+    uint32_t track_id;
+    // true if this detection is starting a new track
+    bool new_track;
+};
+
+struct DetectionMessage
+{
+    uint32_t  ping_num; // same ping number as in FrameHeader
+    uint32_t  num_detections; // number of detections
+    Detection detections[MAX_DETECTIONS_PER_FRAME];
+    
+    DetectionMessage(uint32_t pnum = 0, uint32_t numdetects = 0 )
+    {
+        ping_num = pnum;
+        num_detections = numdetects;
+    };
+    
+}; // Detections
+
+#endif // __NIMS_DETECTIONS_H__
