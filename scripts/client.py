be753209cd9f0b19bc2d5fd4e987cccfc98fc253
d3x389 <d3x389@e0feaedf-03d4-4201-b3fb-2d10bec3ea31>
2015-10-19 22:42:11 +0000

    added ignore property for build and data dirs
    git-svn-id: https://subversion.pnnl.gov/svn/NIMS/trunk@173 e0feaedf-03d4-4201-b3fb-2d10bec3ea31

--- scripts/client.py
+++ scripts/client.py
@@ -7,13 +7,13 @@ A simple echo server
 import socket 
 from time import sleep
 
-#host = '127.0.0.1' 
-host = 'we18320' 
-port = 20001 
-backlog = 5 
+#host = '127.0.0.1'
+host = 'we15128'
+port = 20001
+backlog = 5
 size = 4096
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 
 def hex_to_string(value):
