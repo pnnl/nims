5bcd7c3bd21588e1ada6abe5bea9c39a3ec45ab1
d3x389 <d3x389@e0feaedf-03d4-4201-b3fb-2d10bec3ea31>
2016-02-08 21:42:11 +0000

    added NIMS_HOME
    git-svn-id: https://subversion.pnnl.gov/svn/NIMS/trunk@262 e0feaedf-03d4-4201-b3fb-2d10bec3ea31

--- scripts/test-nims.sh
+++ scripts/test-nims.sh
@@ -14,22 +14,22 @@ sudo rm -rf /dev/shm/*
 # remove log files
 sudo rm -rf /var/tmp/NIMS*
 
+# set environment var
+export NIMS_HOME='/home/shari/Developer/NIMS/build'
 # start nims
 pushd ../build
-./nims > nims.out 2>&1 &
+./nims -l debug > nims.out 2>&1 &
 popd
 
-# follow log files
-#gnome-terminal -e 'tail -F /var/tmp/NIMS-shari-1000/log/nims_0.log' 
-#gnome-terminal -e 'tail -F /var/tmp/NIMS-shari-1000/log/ingester_0.log' 
-#gnome-terminal -e 'tail -F /var/tmp/NIMS-shari-1000/log/tracker_0.log' 
 
 # top left
 gnome-terminal --geometry 124x30+0+0 -x bash -c "tail -f ../build/nims.out | awk '\$4 ~ /ingester/'"
 # bottom left
-gnome-terminal --geometry 124x30+0-0 -x bash -c "tail -f ../build/nims.out | awk '\$4 ~ /tracker/'"
+gnome-terminal --geometry 124x30+0-0 -x bash -c "tail -f ../build/nims.out | awk '\$4 ~ /nims/'"
 # top right
-gnome-terminal --geometry 124x30-0+0 -x bash -c "tail -f ../build/nims.out | awk '\$4 ~ /nims/'"
+gnome-terminal --geometry 124x30-0+0 -x bash -c "tail -f ../build/nims.out | awk '\$4 ~ /detector/'"
+# bottom right
+gnome-terminal --geometry 124x30-0-0 -x bash -c "tail -f ../build/nims.out | awk '\$4 ~ /tracker/'"
 
 # start the webapp
 pushd ../webapp
