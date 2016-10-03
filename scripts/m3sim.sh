1f35515609a00a6f64d5b293a9487a518469fce4
d3x389 <d3x389@e0feaedf-03d4-4201-b3fb-2d10bec3ea31>
2016-10-03 16:55:37 +0000

    added playback with no looping
    git-svn-id: https://subversion.pnnl.gov/svn/NIMS/trunk@333 e0feaedf-03d4-4201-b3fb-2d10bec3ea31

--- scripts/m3sim.sh
+++ scripts/m3sim.sh
@@ -1,1 +1,8 @@
-/home/shari/Developer/NIMS/build/m3sim -r replay -f ../data/2016,Mar,30,15-42-48.imb -r 10 -h 127.0.0.1 -p 20001
+# playback M3 data file with NIMS on nims1
+#/home/shari/Developer/NIMS/build/m3sim -m replay -f ../data/M3/2016,Mar,30,15-42-48.imb -r 10 -h 130.20.41.168 -p 20001 -l true
+
+# playback M3 data file with NIMS on dev system
+#/home/shari/Developer/NIMS/build/m3sim -m replay -f ../data/M3/2016,Mar,30,15-42-48.imb -r 10 -h 127.0.0.1 -p 20001 -l true
+
+# playback simulated data file, no looping
+/home/shari/Developer/NIMS/build/m3sim -m replay -f ../data/sim_bg50-10_tarrng.imb -r 10 -h 127.0.0.1 -p 20001
